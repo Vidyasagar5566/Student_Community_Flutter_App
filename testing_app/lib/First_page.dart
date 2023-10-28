e2a30b3631f6ddf3e073a2710395ac7ec8b534a4
Vidyasagar5566 <brvsagar5566@gmail.com>
2023-10-28 13:57:01 +0530

    dw

--- testing_app/lib/First_page.dart
+++ testing_app/lib/First_page.dart
@@ -1,5 +1,4 @@
 // ignore_for_file: prefer_const_constructors
-
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:testing_app/Activities/Models.dart';
