e3d37f94fcbd21a44a3ada030f1aea304c2a95e6
Vidyasagar5566 <brvsagar5566@gmail.com>
2023-11-01 00:15:49 +0530

    ds

--- testing_app/lib/Messanger/messanger.dart
+++ testing_app/lib/Messanger/messanger.dart
@@ -1,5 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import '/Fcm_Notif_Domains/Servers.dart';
+import '../First_page.dart';
+import '/Fcm_Notif_Domains/servers.dart';
 import 'Servers.dart';
 import 'Models.dart';
 import '/User_profile/Models.dart';
@@ -9,7 +11,8 @@ import 'package:video_player/video_player.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:image_picker/image_picker.dart';
 import '../Files_disply_download/pdf_videos_images.dart';
-import 'Search_bar.dart';
+import 'chatroom.dart';
+import 'search_bar.dart';
 import 'Single_message.dart';
 import '/User_Star_Mark/User_Profile_Star_Mark.dart';
 
@@ -27,6 +30,8 @@ class messanger extends StatefulWidget {
 }
 
 class _messangerState extends State<messanger> {
+  List<String> uids = [];
+  List<String> messages = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -56,43 +61,127 @@ class _messangerState extends State<messanger> {
           ],
           backgroundColor: Colors.white70,
         ),
-        body: FutureBuilder<List<Messanger>>(
-          future: messanger_servers().get_messages_list(),
-          builder: (ctx, AsyncSnapshot snapshot) {
-            if (snapshot.connectionState == ConnectionState.done) {
-              if (snapshot.hasError) {
-                return Center(
-                  child: Text(
-                    '${snapshot.error} occurred',
-                    style: TextStyle(fontSize: 18),
-                  ),
-                );
-              } else if (snapshot.hasData) {
-                List<Messanger> last_user_messages = snapshot.data;
-                if (last_user_messages.isEmpty) {
-                  return Container(
-                      margin: EdgeInsets.all(30),
-                      padding: EdgeInsets.all(30),
-                      child: const Text("No conversations started yet",
-                          style: TextStyle(
-                              fontWeight: FontWeight.w500, fontSize: 24)));
+        body: StreamBuilder(
+            stream: FirebaseFirestore.instance
+                .collection("chatrooms")
+                .where("participants.${app_user.userUuid}", isEqualTo: true)
+                .snapshots(),
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.active) {
+                if (snapshot.hasData) {
+                  QuerySnapshot chatroomsnapshot =
+                      snapshot.data as QuerySnapshot;
+                  for (int index = 0;
+                      index < chatroomsnapshot.docs.length;
+                      index = index + 1) {
+                    ChatRoomModel chatroommodel = ChatRoomModel.FromMap(
+                        chatroomsnapshot.docs[index].data()
+                            as Map<String, dynamic>);
+                    String? lastmessage = chatroommodel.lastmessage;
+                    Map<String, dynamic> participants =
+                        chatroommodel.participants!;
+                    List<String> participantKeys = participants.keys.toList();
+                    participantKeys.remove(app_user.userUuid);
+                    int flag = 0;
+                    int uidIndex = 0;
+                    for (int i = 0; i < uids.length; i++) {
+                      if (uids[i] == participantKeys[0]) {
+                        flag = 1;
+                        uidIndex = i;
+                      }
+                    }
+                    if (flag == 0) {
+                      uids.add(participantKeys[0]);
+                      if (lastmessage != null) {
+                        messages.add(lastmessage);
+                      }
+                    } else {
+                      if (lastmessage != null) {
+                        messages[uidIndex] = lastmessage;
+                      }
+                    }
+                  }
+
+                  for (int i = 0; i < uids.length; i++) {
+                    print("Uid" + uids[i]);
+                  }
+
+                  return fireBaseUuids_to_backendUsers(
+                      widget.app_user, messages, uids);
+                } else if (snapshot.hasError) {
+                  return Center(
+                    child: Text(snapshot.error.toString()),
+                  );
                 } else {
-                  return messanger1(widget.app_user, last_user_messages);
+                  return Center(
+                    child: Text("No Conversations Yet!"),
+                  );
                 }
+              } else {
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
               }
-            }
-            return const Center(
-              child: CircularProgressIndicator(),
+            }));
+  }
+}
+
+class fireBaseUuids_to_backendUsers extends StatefulWidget {
+  Username app_user;
+  List<String> user_messages;
+  List<String> user_uuids;
+  fireBaseUuids_to_backendUsers(
+      this.app_user, this.user_messages, this.user_uuids);
+
+  @override
+  State<fireBaseUuids_to_backendUsers> createState() =>
+      _fireBaseUuids_to_backendUsersState();
+}
+
+class _fireBaseUuids_to_backendUsersState
+    extends State<fireBaseUuids_to_backendUsers> {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<List<SmallUsername>>(
+      future: messanger_servers()
+          .get_fire_base_uuids_to_backend_users(widget.user_uuids.join('#')),
+      builder: (ctx, AsyncSnapshot snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          if (snapshot.hasError) {
+            return Center(
+              child: Text(
+                '${snapshot.error} occurred',
+                style: TextStyle(fontSize: 18),
+              ),
             );
-          },
-        ));
+          } else if (snapshot.hasData) {
+            List<SmallUsername> message_users = snapshot.data;
+            if (message_users.isEmpty) {
+              return Container(
+                  margin: EdgeInsets.all(30),
+                  padding: EdgeInsets.all(30),
+                  child: const Text("No conversations started yet",
+                      style: TextStyle(
+                          fontWeight: FontWeight.w500, fontSize: 24)));
+            } else {
+              return messanger1(
+                  widget.app_user, widget.user_messages, message_users);
+            }
+          }
+        }
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
   }
 }
 
 class messanger1 extends StatefulWidget {
   Username app_user;
-  List<Messanger> last_user_messages;
-  messanger1(this.app_user, this.last_user_messages);
+  List<String> user_messages;
+  List<SmallUsername> message_users;
+  messanger1(this.app_user, this.user_messages, this.message_users);
 
   @override
   State<messanger1> createState() => _messanger1State();
@@ -120,36 +209,46 @@ class _messanger1State extends State<messanger1> {
               ]),
               const SizedBox(height: 10),
               ListView.builder(
-                  itemCount: widget.last_user_messages.length,
+                  itemCount: widget.message_users.length,
                   shrinkWrap: true,
                   padding: EdgeInsets.zero,
                   physics: NeverScrollableScrollPhysics(),
                   itemBuilder: (BuildContext context, int index) {
-                    var message_user;
-                    Messanger message = widget.last_user_messages[index];
-                    if (widget.app_user.email ==
-                        message.messageReceiver!.email) {
-                      message_user = message.messageSender!;
-                    } else {
-                      message_user = message.messageReceiver!;
-                    }
-                    return _buildLoadingScreen(message_user, message, index);
+                    SmallUsername message_user = widget.message_users[index];
+
+                    return _buildLoadingScreen(
+                        message_user, widget.user_messages[index], index);
                   }),
             ],
           )),
     );
   }
 
+  Future<ChatRoomModel?> getChatRoomModel(SmallUsername targetuser) async {
+    QuerySnapshot snapshot = await FirebaseFirestore.instance
+        .collection("chatrooms")
+        .where("participants.${widget.app_user.userUuid}", isEqualTo: true)
+        .where("participants.${targetuser.userUuid}", isEqualTo: true)
+        .get();
+    var docData = snapshot.docs[0].data();
+    ChatRoomModel? existingchatroom =
+        ChatRoomModel.FromMap(docData as Map<String, dynamic>);
+
+    return existingchatroom;
+  }
+
   Widget _buildLoadingScreen(
-      SmallUsername message_user, Messanger message, int index) {
+      SmallUsername message_user, String user_message, int index) {
     var width = MediaQuery.of(context).size.width;
 
     return GestureDetector(
-      onTap: () {
-        Navigator.of(context)
-            .push(MaterialPageRoute(builder: (BuildContext context) {
-          return messages_viewer(widget.app_user, message_user, []);
-        }));
+      onTap: () async {
+        ChatRoomModel? chatroomModel = await getChatRoomModel(message_user);
+        if (chatroomModel != null) {
+          Navigator.push(context, MaterialPageRoute(builder: (context) {
+            return chatroom(targetuser: message_user, chatRoom: chatroomModel);
+          }));
+        }
       },
       child: Column(
         children: [
@@ -219,20 +318,12 @@ class _messanger1State extends State<messanger1> {
                   const SizedBox(
                     height: 10,
                   ),
-                  message.messageSeen!
-                      ? Text(
-                          "message : " + utf8convert(message.messageBody!),
-                          softWrap: false, maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                          //post.description,,
-                        )
-                      : Text(
-                          "message : " + utf8convert(message.messageBody!),
-                          softWrap: false, maxLines: 1,
-                          overflow: TextOverflow.ellipsis,
-                          style: const TextStyle(fontWeight: FontWeight.w600),
-                          //post.description,,
-                        )
+                  Text(
+                    "message : " + utf8convert(user_message),
+                    softWrap: false, maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    //post.description,,
+                  )
                 ],
               )),
         ],
